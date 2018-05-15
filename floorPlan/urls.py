7c16665324d7b1b45421ed93cb02d3b8ffa9c7e4
Nora Baltus <n.baltus@student.tue.nl>
2018-05-15 12:18:49 +0200

    Push pull

--- floorPlan/urls.py
+++ floorPlan/urls.py
@@ -6,8 +6,9 @@ app_name = "floorPlan"
 urlpatterns = [
     re_path(r'^room/(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='room-detail'),
     re_path(r'^room/(?P<pk>[0-9]+)/delete/$', views.RoomDelete.as_view(), name='room-delete'),
-    re_path(r'creator/$', views.sandbox, name='sandbox'),
-    re_path(r'add_user/$', views.UserFormView.as_view(), name='user-add'),
+    re_path(r'creator/$', 'views.sandbox', name='sandbox'),
+dhdhdhy
+    re_path(r'creator/add_user/$', views.UserFormView.as_view(), name='person-add'),
     re_path(r'creator/add_room/$', views.RoomCreate.as_view(), name='room-add'),
     re_path(r'creator/add_table/$', views.TableCreate.as_view(), name='table-add'),
     re_path(r'creator/add_chair/$', views.ChairCreate.as_view(), name='chair-add'),
