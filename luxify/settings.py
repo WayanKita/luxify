98859734bd4f86d900feded9cc64da00c4d5a9e6
WayanKita <Wayantraversat@hotmail.com>
2018-05-15 11:07:02 +0200

    gitignore

--- luxify/settings.py
+++ luxify/settings.py
@@ -65,11 +65,8 @@ WSGI_APPLICATION = 'luxify.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'deeznuts',
-        'USER': 'postgres',
-        'PASSWORD': '290394',
-        'HOST': 'localhost',
+       'ENGINE': 'django.db.backends.sqlite3',
+       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
