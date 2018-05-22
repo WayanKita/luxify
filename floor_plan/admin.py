750243d7b2bf1866fc6c58c771c3b101a3f6d123
WayanKita <Wayantraversat@hotmail.com>
2018-05-22 09:07:52 +0200

    reverted old version 2

--- floor_plan/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from floor_plan.models import *
-
-admin.site.register(Room)
-admin.site.register(Table)
-admin.site.register(Participant)
-admin.site.register(MyUser)
--- floor_plan/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class FloorPlanConfig(AppConfig):
-    name = 'floor_plan'
