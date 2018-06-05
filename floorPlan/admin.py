2cee349703499b6c2d407eaed46f6f77407c847d
Nora Baltus <n.baltus@student.tue.nl>
2018-06-05 11:12:48 +0200

    Improved floor plan admin a bit

--- floorPlan/admin.py
+++ floorPlan/admin.py
@@ -1,14 +1,33 @@
 from django.contrib import admin
 from .models import *
 
+class WindowInline(admin.TabularInline):
+	model = Window
+
+class DoorInline(admin.TabularInline):
+	model = Door
+
+class ChairInline(admin.TabularInline):
+	model = Chair
+
+class DeskInline(admin.TabularInline):
+	model = Desk
+
+class RoomAdmin(admin.ModelAdmin):
+    inlines = [
+	WindowInline,
+	DoorInline,
+	DeskInline,
+    ]
+
 # Allows the admin to edit thee following tables on url [...]/admin
-admin.site.register(Room)
-admin.site.register(Desk)
+admin.site.register(Room, RoomAdmin)
+# admin.site.register(Desk)
 admin.site.register(Participant)
 admin.site.register(Sensor)
-admin.site.register(Window)
+# admin.site.register(Window)
 admin.site.register(Chair)
-admin.site.register(Door)
+# admin.site.register(Door)
 admin.site.register(AlertnessQuestionnaire)
 admin.site.register(DemographicQuestionnaire)
 admin.site.register(Analytics)
