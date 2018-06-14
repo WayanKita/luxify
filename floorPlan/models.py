e84c46a3b7eea36aa7b1eb1994a97f57a37e5dd8
Nora <n.baltus@student.tue.nl>
2018-06-14 08:11:04 +0200

    Set default user chair and room to null because these values are not hardcoded

--- floorPlan/models.py
+++ floorPlan/models.py
@@ -121,8 +121,8 @@ class Participant(models.Model):                                        # User o
     username = models.OneToOneField(User, on_delete=models.CASCADE)   # username                        # password
     survey_done = models.BooleanField(default=False)                    # demographicStatus ; not used
     in_workspace = models.BooleanField(default=False)                   # demographicStatus ; not used
-    room = models.IntegerField(blank=True, null=True, default=null)        # roomID
-    chair = models.IntegerField(blank=True, null=True, default=null)        # deskID
+    room = models.IntegerField(blank=True, null=True, default=None)        # roomID
+    chair = models.IntegerField(blank=True, null=True, default=None)        # deskID
     profile = models.IntegerField(blank=True, null=True, default=1)        # deskID
     user_category = models.IntegerField(blank=True, null=True, default=0)
 
