80c5beb85c659440fe5da072530fb63258775044
Nora <n.baltus@student.tue.nl>
2018-06-15 08:05:26 +0200

    Hopefully fixed float issue

--- sync/tasks.py
+++ sync/tasks.py
@@ -25,8 +25,8 @@ def task_number_one():
 									Sensor.objects.create(column_number=idx, sensor_name=field)
 						if not "Timestamp" in row:
 							for desk in desks:
-								desk.illuminance = row[desk.illuminance_sensor.column_number]
-								desk.occupancy = row[desk.occupancy_sensor.column_number]
+								desk.illuminance = float(row[desk.illuminance_sensor.column_number])
+								desk.occupancy = int(row[desk.occupancy_sensor.column_number])
 								desk.save()
 								Sensor_History.objects.create(desk=desk, time_stamp=timezone.now(), light_value=row[desk.illuminance_sensor.column_number], occupancy_value=row[desk.occupancy_sensor.column_number])
 					os.rename(path + '/' + file, path + '/archives/' + file)
