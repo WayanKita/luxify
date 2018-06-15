cf4c577810ab40745916e9e5c10eee873036bd2a
Nora <n.baltus@student.tue.nl>
2018-06-15 08:42:33 +0200

    Made tasks.py prettier

--- sync/tasks.py
+++ sync/tasks.py
@@ -25,9 +25,11 @@ def task_number_one():
 									Sensor.objects.create(column_number=idx, sensor_name=field)
 						if not "Timestamp" in row:
 							for desk in desks:
-								if row[desk.illuminance_sensor.column_number].isdigit() and row[desk.occupancy_sensor.column_number].isdigit():
+								illuminance_column = desk.illuminance_sensor.column_number
+								occupancy_column = desk.occupancy_sensor.column_number
+								if row[illuminance_column].isdigit() and row[occupancy_column].isdigit():
 									desk.illuminance = float(row[desk.illuminance_sensor.column_number])
 									desk.occupied = int(row[desk.occupancy_sensor.column_number])
 									desk.save()
-									Sensor_History.objects.create(desk=desk, time_stamp=timezone.now(), light_value=row[desk.illuminance_sensor.column_number], occupancy_value=row[desk.occupancy_sensor.column_number])
+									Sensor_History.objects.create(desk=desk, time_stamp=timezone.now(), light_value=illuminance_column, occupancy_value=occupancy_sensor)
 					os.rename(path + '/' + file, path + '/archives/' + file)
