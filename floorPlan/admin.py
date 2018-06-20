b60ffc814950b13d2bed319eafe4be73dec6dca1
Nora Baltus <nora@wlan-187105.nbw.tue.nl>
2018-06-20 10:43:30 +0200

    Revert read-only attempt

--- floorPlan/admin.py
+++ floorPlan/admin.py
@@ -65,20 +65,12 @@ class SensorAdmin(admin.ModelAdmin):
     def has_add_permission(self, request):
         return False
 
-    def has_change_permission(self, request, obj=None):
-        if request.user.is_staff:
-            return False
-
 
 class SensorHistoryAdmin(admin.ModelAdmin):
     actions = [download_sensor_history]
 
     def has_add_permission(self, request):
         return False
-
-    def has_change_permission(self, request, obj=None):
-        if request.user.is_staff:
-            return False
 
 
 class AlertnessQuestionnaireAdmin(admin.ModelAdmin):
