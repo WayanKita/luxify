1719f089f68dad14bff21ccebba15bb758b651ff
Nora Baltus <nora@wlan-187105.nbw.tue.nl>
2018-06-20 09:58:19 +0200

    Alertness questionnaire download'

--- floorPlan/admin.py
+++ floorPlan/admin.py
@@ -1,8 +1,32 @@
 from django.contrib import admin
+from django.http import HttpResponse
 from .models import *
 
 # TODO: remove edit rights to views in admin panel
 # TODO: fix typo on inlines
+
+def do_download(model, all_objects):
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename={}.csv'.format('test')
+    writer = csv.writer(response)
+
+    fields = [field for field in model.get_fields() if not field.many_to_many and not field.one_to_many]
+   
+    writer.writerow([field.name for field in fields])
+   
+    for desk in all_objects:
+        row = []
+        for field in fields:
+            field_value = getattr(desk, field.name)
+            row.append(field_value)
+
+        writer.writerow(row)
+    return response
+
+
+def download_alertness_questionnaire(request):
+    return do_download(AlertnessQuestionnaire._meta, AlertnessQuestionnaire.objects.all())
+
 
 class WindowInline(admin.TabularInline):
     model = Window
@@ -28,8 +52,9 @@ class SensorAdmin(admin.ModelAdmin):
     def has_add_permission(self, request):
         return False
 
-
 class AlertnessQuestionnaireAdmin(admin.ModelAdmin):
+    actions = [download_alertness_questionnaire]
+    
     def has_add_permission(self, request):
         return False
 
