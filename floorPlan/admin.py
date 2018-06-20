d11a88a8f128be8386287c8b226ed056a57ba7be
Nora Baltus <nora@wlan-187105.nbw.tue.nl>
2018-06-20 09:30:16 +0200

    Demographic questionnaire

--- floorPlan/admin.py
+++ floorPlan/admin.py
@@ -34,13 +34,18 @@ class AlertnessQuestionnaireAdmin(admin.ModelAdmin):
         return False
 
 
+class DemographicQuestionnaireAdmin(admin.ModelAdmin):
+    def has_add_permission(self, request):
+        return False
+
+
 # Allows the admin to edit thee following tables on url [...]/admin
 admin.site.register(Room, RoomAdmin)
 admin.site.register(Participant)
 admin.site.register(Sensor, SensorAdmin)  # sensors are automatically created
 admin.site.register(SensorHistory)
 admin.site.register(AlertnessQuestionnaire, AlertnessQuestionnaireAdmin)
-admin.site.register(DemographicQuestionnaire)
+admin.site.register(DemographicQuestionnaire, DemographicQuestionnaireAdmin)
 admin.site.register(ParticipantProfiles)
 admin.site.register(Analytics)
 admin.site.register(UserCategory)
