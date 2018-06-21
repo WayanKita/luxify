8bb2c3de4a534b1fbc5fc63b19a3f273b44d30d9
Nora Baltus <nora@wlan-187041.nbw.tue.nl>
2018-06-21 14:03:39 +0200

    Admin changes

--- floorPlan/admin.py
+++ floorPlan/admin.py
@@ -109,7 +109,8 @@ class ParticipantAdmin(admin.ModelAdmin):
 
 class UserProfileAdmin(UserAdmin): 
     def get_form(self, request, obj=None, **kwargs):
-        if obj and request.user.is_staff:
+        if obj:
+            if request.user.is_staff:
                 self.list_display = ('username')
 
             return super(UserAdmin, self).get_form(request, obj, **kwargs)
