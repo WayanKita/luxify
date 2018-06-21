27374f96620644f7446730b9c5b670b349c6d29b
Nora Baltus <nora@wlan-187041.nbw.tue.nl>
2018-06-21 11:34:52 +0200

    Admin changes

--- floorPlan/admin.py
+++ floorPlan/admin.py
@@ -105,11 +105,6 @@ class ParticipantAdmin(admin.ModelAdmin):
         return False
 
     def has_delete_permission(self, request, obj=None):
-        url = '/home/group10user/luxify/sync/test.txt'
-        handle = open(url, 'r+')
-        handle = write(obj)
-        handle.close()
-
         if request.user.is_staff:
             return False
 
