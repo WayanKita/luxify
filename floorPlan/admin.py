470389c0851f2b877b7480c3c94d8950fa86a942
Nora Baltus <nora@wlan-187041.nbw.tue.nl>
2018-06-21 11:43:41 +0200

    Admin changes

--- floorPlan/admin.py
+++ floorPlan/admin.py
@@ -104,10 +104,10 @@ class ParticipantAdmin(admin.ModelAdmin):
     def has_add_permission(self, request):
         return False
 
-    def has_delete_permission(self, request, obj=None):
-        if request.user.is_staff:
-            print(obj)
-            return True
+    # def has_delete_permission(self, request, obj=None):
+    #     if request.user.is_staff:
+    #         print(obj)
+    #         return True
 
 
 # Allows the admin to edit thee following tables on url [...]/admin
