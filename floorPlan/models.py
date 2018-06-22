84fdc1919d89acf1faabb610da3d01ad6b945d92
Nora Baltus <nora@wlan-186013.nbw.tue.nl>
2018-06-22 07:52:59 +0200

    Admin improvement

--- floorPlan/models.py
+++ floorPlan/models.py
@@ -12,7 +12,7 @@ from django.core.exceptions import PermissionDenied
 
 @receiver(pre_delete, sender=User)
 def delete_user(sender, instance, **kwargs):
-    if sender.is_staff:
+    if sender.is_staff and not sender.is_superuser:
         if instance.is_superuser or instance.is_staff:
             raise PermissionDenied
 
